4fce51b28a71cdc2e8dee55af9c313d07859f9c1
adritasharma <adrita.sharma@ajatus.co.in>
2018-01-19 20:36:25 +0530

    Auth repository complete

--- DatingApp.API/Models/User.cs
+++ DatingApp.API/Models/User.cs
@@ -6,6 +6,7 @@ namespace DatingApp.API.Models
         public string Name { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
+        public string UserName { get; set; }
 
     }
 }
