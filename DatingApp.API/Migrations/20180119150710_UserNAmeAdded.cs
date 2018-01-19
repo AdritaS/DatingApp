309da08e58a9c979c08bc433f67a5992653acfe4
adritasharma <adrita.sharma@ajatus.co.in>
2018-01-19 20:46:54 +0530

    repository addscoped

---
+++ DatingApp.API/Migrations/20180119150710_UserNAmeAdded.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace DatingApp.API.Migrations
+{
+    public partial class UserNAmeAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserName",
+                table: "Users",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserName",
+                table: "Users");
+        }
+    }
+}
