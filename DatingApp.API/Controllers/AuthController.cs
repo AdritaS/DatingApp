95ed160eac1292e763193e863ed50e535d03ccf5
adritasharma <adrita.sharma@ajatus.co.in>
2018-01-20 22:09:51 +0530

    Register controller complete

---
+++ DatingApp.API/Controllers/AuthController.cs
@@ -0,0 +1,36 @@
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using DatingApp.API.Dtos;
+using DatingApp.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DatingApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class AuthController : Controller
+    {
+        private readonly IAuthRepository _repo;
+        public AuthController(IAuthRepository repo)
+        {
+            _repo = repo;
+            
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] UserForRegisterDto userForRegisterDto)
+        {
+            userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
+
+            if(await _repo.UserExists(userForRegisterDto.Username))
+                return BadRequest("Username is Taken");
+
+            var userToCreate = new User {
+                UserName = userForRegisterDto.Username
+            };
+
+            var createUser = await _repo.Register(userToCreate, userForRegisterDto.Password);    
+
+            return StatusCode(201);
+        }
+    }
+}
