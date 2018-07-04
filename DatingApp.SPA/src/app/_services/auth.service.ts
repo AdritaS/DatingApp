1910c9df27abdcfd19795b418bc9ef8ba1d02b56
Adrita <Adrita@DESKTOP-NJOPU8B>
2018-07-04 19:47:46 +0530

    registration working

--- DatingApp.SPA/src/app/_services/auth.service.ts
+++ DatingApp.SPA/src/app/_services/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http,Response, Headers,RequestOptions } from '@angular/http';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/map';
 
@@ -7,22 +7,26 @@ import 'rxjs/add/operator/map';
 export class AuthService {
 
     baseUrl = "http://localhost:5000/api/auth/"
-    userToken : any
+    userToken: any
     constructor(private http: Http) { }
 
 
-    login(model : any)
-    {
+    login(model: any) {
+        return this.http.post(this.baseUrl + "login", model, this.requestOptions()).map((response: Response) => {
+            const user = response.json()
+
+            this.userToken = user.userToken
+            console.log(user)
+            localStorage.setItem('userToken', this.userToken);
+        })
+    }
+
+    register(model: any) {
+        return this.http.post(this.baseUrl + "register", model, this.requestOptions())
+    }
+    private requestOptions() {
         const headers = new Headers({ 'Content-Type': 'application/json; charset=UTF-8' });
-        const options = new RequestOptions({ headers: headers });
-
-        return this.http.post(this.baseUrl + "login" ,model,options).map((response: Response) => {
-           const user = response.json()
-
-           this.userToken = user.userToken
-              console.log(user)
-              localStorage.setItem('userToken', this.userToken);
-            })
+        return new RequestOptions({ headers: headers });
     }
 
 }
